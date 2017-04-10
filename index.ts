9d1423c64156e3668e1b0f71e0ad1c79cdfb00fe
Morlay <morlay@qq.com>
2017-04-11 00:27:06 +0800

    added get list and delete by name

--- index.ts
+++ index.ts
@@ -1,35 +1,68 @@
-import * as logger from "koa-logger";
-import * as serve from "koa-static";
-import * as cors from "koa-cors";
-import * as Koa from "koa";
-import * as path from "path";
-import * as fs from "fs";
-import * as parse from "async-busboy";
+import * as parse from "async-busboy"
+import * as fse from "fs-extra"
+import * as Koa from "koa"
+import * as cors from "koa-cors"
+import * as logger from "koa-logger"
+import * as Router from "koa-router"
+import * as serve from "koa-static"
+import * as path from "path"
 
-const app = new Koa();
+const app = new Koa()
+const router = new Router()
 
-app.use(cors());
-app.use(logger());
+app.use(cors())
+app.use(logger())
 
-app.use(serve(path.join(__dirname, "/public")));
+app.use(serve(path.join(__dirname, "/public")))
 
-app.use(async(ctx, next) => {
-  if ("POST" !== ctx.method) {
-    return next();
-  }
+const fileRootFolder = path.join(__dirname, "public/files")
 
-  const { files } = await parse(ctx.req);
+// 上传文件
+router.post("/files", async (ctx) => {
+  const { files } = await parse(ctx.req)
+  const file = files[0]
 
-  files.forEach((file) => {
-    if (file.filename) {
-      const stream = fs.createWriteStream(path.join(__dirname, "public", path.basename(file.filename)));
-      file.pipe(stream);
-      console.log('uploading %s -> %s', file.filename, stream.path);
+  if (file.filename) {
+    const stream = fse.createWriteStream(path.join(fileRootFolder, path.basename(file.filename)))
+    file.pipe(stream)
+    console.log("uploading %s -> %s", file.filename, stream.path)
+
+    ctx.body = {
+      name: file.filename,
     }
-  });
+  } else {
+    ctx.status = 400
+    ctx.body = {
+      error: "上传文件不能为空",
+    }
+  }
+})
 
-  ctx.status = 204;
-});
+// 获取文件列表
+router.get("/files", async (ctx) => {
+  const files = fse.readdirSync(fileRootFolder)
 
-app.listen(3000);
-console.log("listening on port 3000");
+  ctx.body = {
+    files: files.map((name: string) => ({ name })),
+  }
+})
+
+// 删除单个文件
+router.delete("/files/:name", async (ctx) => {
+  try {
+    fse.removeSync(path.join(fileRootFolder, ctx.params.name))
+    ctx.status = 204
+  } catch (e) {
+    console.log(e)
+    ctx.status = 404
+    ctx.body = {
+      error: "没有该文件",
+    }
+  }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
+console.log("listening on port 3000")
